998dc050fdfb3df96dc1c7393e55aa968c90807d
shimizu <noon3m.op.sh@gmail.com>
2025-04-06 01:22:28 +0900

    [change] ファイル名の修正とカテゴリを一部修正

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,21 @@ import {
 import type { MenuProps } from "antd";
 
 import AppRoutes from "@/router";
-import Header from "@/component/header/Header";
+import Header from "@/component/header";
 
 type MenuItem = Required<MenuProps>["items"][number];
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false); // headerのメニューが開いているかどうか
-  const [selectedCategoryKey, setSelectedCategoryKey] =
-    useState<string>("すべて"); // 選択されているカテゴリ
+  const [selectedCategoryKey, setSelectedCategoryKey] = useState<string>("all"); // 選択されているカテゴリ
 
   // TODO: menuItems(カテゴリ)をAPIから取得
   const menuItems: MenuItem[] = [
-    { key: "すべて", icon: <AppstoreOutlined />, label: "すべて" },
+    { key: "all", icon: <AppstoreOutlined />, label: "すべて" },
     { key: "交通", icon: <CarOutlined />, label: "交通" },
-    { key: "スポーツ", icon: <CoffeeOutlined />, label: "スポーツ" },
+    { key: "sports", icon: <CoffeeOutlined />, label: "スポーツ" },
     { key: "施設", icon: <CoffeeOutlined />, label: "施設" },
-    { key: "自然", icon: <CoffeeOutlined />, label: "自然" },
+    { key: "nature", icon: <CoffeeOutlined />, label: "自然" },
   ];
 
   // カテゴリを選択した時の処理
