08d86109971e73f8d7e97de9a3f936ff96cd78a7
shimizu <noon3m.op.sh@gmail.com>
2025-04-06 02:26:28 +0900

    [change] Appコンポーネントからヘッダーとカテゴリ管理のロジックを削除し、AppRoutes内に新しいAppContentコンポーネントを追加。ルーティングとメニュー表示の管理を統合。

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,10 @@
-import { useState, useCallback } from "react";
-import {
-  AppstoreOutlined,
-  CarOutlined,
-  CoffeeOutlined,
-} from "@ant-design/icons";
-import type { MenuProps } from "antd";
-
 import AppRoutes from "@/router";
-import Header from "@/component/header";
-
-type MenuItem = Required<MenuProps>["items"][number];
 
 function App() {
-  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false); // headerのメニューが開いているかどうか
-  const [selectedCategoryKey, setSelectedCategoryKey] = useState<string>("all"); // 選択されているカテゴリ
-
-  // TODO: menuItems(カテゴリ)をAPIから取得
-  const menuItems: MenuItem[] = [
-    { key: "all", icon: <AppstoreOutlined />, label: "すべて" },
-    { key: "交通", icon: <CarOutlined />, label: "交通" },
-    { key: "sports", icon: <CoffeeOutlined />, label: "スポーツ" },
-    { key: "施設", icon: <CoffeeOutlined />, label: "施設" },
-    { key: "nature", icon: <CoffeeOutlined />, label: "自然" },
-  ];
-
-  // カテゴリを選択した時の処理
-  const handleCategorySelect = useCallback((key: string) => {
-    setSelectedCategoryKey(key);
-    setIsMenuOpen(false);
-  }, []);
-
   return (
-    <>
-      <Header
-        isMenuOpen={isMenuOpen}
-        setIsMenuOpen={setIsMenuOpen}
-        menuItems={menuItems}
-        onCategorySelect={handleCategorySelect}
-      />
-      <AppRoutes
-        selectedCategoryKey={selectedCategoryKey}
-        onCategorySelect={handleCategorySelect}
-      />
-    </>
+    <div className="flex h-full flex-col">
+      <AppRoutes />
+    </div>
   );
 }
 
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import {
+  AppstoreOutlined,
+  CarOutlined,
+  CoffeeOutlined,
+} from "@ant-design/icons";
+import { MenuProps } from "antd";
+
 import { HomePage } from "@/feature/home/pages";
 import { UpLoadPage, AdminHomePage } from "@/feature/admin/pages";
 import NotFound from "@/component/notfound";
 import { AuthProvider } from "@/feature/admin/components/AuthProvider";
 import RequireAuth from "@/feature/admin/components/RequireAuth";
+import Header from "@/component/header";
+import { useCallback, useEffect, useState } from "react";
+import { useAuth } from "@/feature/admin/hook/useAuth";
 
 const routes = [
   {
@@ -26,40 +36,87 @@ const routes = [
     Component: NotFound,
     isProtected: false,
   },
-  // ] as const satisfies RouteProps[];
 ];
 
-const AppRoutes = ({
-  selectedCategoryKey, // 選択されているカテゴリ
-  onCategorySelect, // カテゴリを選択した時の処理(カテゴリーのステートを更新してる)
-}: {
-  selectedCategoryKey: string;
-  onCategorySelect: (key: string) => void;
-}) => {
+type MenuItem = Required<MenuProps>["items"][number];
+
+const AppContent = () => {
+  const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const [selectedCategoryKey, setSelectedCategoryKey] = useState<string>("all");
+  const { setIsAdmin } = useAuth();
+
+  const menuItems: MenuItem[] = [
+    { key: "all", icon: <AppstoreOutlined />, label: "すべて" },
+    { key: "交通", icon: <CarOutlined />, label: "交通" },
+    { key: "sports", icon: <CoffeeOutlined />, label: "スポーツ" },
+    { key: "施設", icon: <CoffeeOutlined />, label: "施設" },
+    { key: "nature", icon: <CoffeeOutlined />, label: "自然" },
+  ];
+
+  const handleCategorySelect = useCallback((key: string) => {
+    setSelectedCategoryKey(key);
+    setIsMenuOpen(false);
+  }, []);
+
+  useEffect(() => {
+    // 現在のパスが/adminで始まるかどうかをチェック
+    const isAdminRoute = location.pathname.startsWith("/admin");
+
+    // not found判定
+    const isKnownRoute = routes.some((route) => {
+      if (route.path === "*") return false;
+      return (
+        location.pathname === route.path ||
+        (route.path.includes("*") &&
+          location.pathname.startsWith(route.path.replace("*", "")))
+      );
+    });
+    // not found時はtrue
+    const isNotFoundRoute = !isKnownRoute;
+    setIsAdmin(isAdminRoute || isNotFoundRoute);
+  }, [location.pathname, setIsAdmin]);
+
+  return (
+    <>
+      <Header
+        isMenuOpen={isMenuOpen}
+        setIsMenuOpen={setIsMenuOpen}
+        menuItems={menuItems}
+        onCategorySelect={handleCategorySelect}
+      />
+
+      <Routes>
+        {routes.map(({ path, Component, isProtected }, i) => {
+          const element = (
+            <Component
+              selectedCategoryKey={selectedCategoryKey}
+              onCategorySelect={handleCategorySelect}
+            />
+          );
+          return (
+            <Route
+              key={i}
+              path={path}
+              element={
+                isProtected ? <RequireAuth>{element}</RequireAuth> : element
+              }
+            />
+          );
+        })}
+      </Routes>
+    </>
+  );
+};
+
+const AppRoutes = () => {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Routes>
-          {routes.map(({ path, Component, isProtected }, i) => {
-            const element = (
-              <Component
-                selectedCategoryKey={selectedCategoryKey} // 選択されているカテゴリ
-                onCategorySelect={onCategorySelect}
-              />
-            );
-            return (
-              <Route
-                key={i}
-                path={path}
-                element={
-                  isProtected ? <RequireAuth>{element}</RequireAuth> : element
-                }
-              />
-            );
-          })}
-        </Routes>
+        <AppContent />
       </BrowserRouter>
     </AuthProvider>
   );
 };
+
 export default AppRoutes;
